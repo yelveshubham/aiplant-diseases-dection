5dc060d8ac560b3d94973e66598976847fcf569e
yelveshubham <103681201+yelveshubham@users.noreply.github.com>
2022-04-15 09:04:06 -0300

    Add files via upload

---
+++ src/components/Homepage/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="copyright">
+      © 2021 Plant AI
+      </p>
+      {/* <div className="link-group">
+        <h1 className="linkgroup-title">Link Group 1</h1>
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+      </div>
+      <div className="link-group">
+        <h1 className="linkgroup-title">Link Group 1</h1>
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+      </div>
+      <div className="link-group">
+        <h1 className="linkgroup-title">Link Group 1</h1>
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+        <FooterLink linkTitle="Link Title" link="#" />
+      </div> */}
+    </div>
+    
+  );
+};
+
+export default Footer;
